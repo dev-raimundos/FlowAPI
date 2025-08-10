dbf1d8d4847d15cdb7420b8255bf517149082121
dev-raimundos <devraimundos@outlook.com>
2025-08-10 16:48:17 -0300

    FEAT: Adicionando transações e logs

--- src/main/java/com/devraimundos/FlowAPI/business/services/TransacaoService.java
+++ src/main/java/com/devraimundos/FlowAPI/business/services/TransacaoService.java
@@ -1,15 +1,27 @@
 package com.devraimundos.FlowAPI.business.services;
 
 import com.devraimundos.FlowAPI.controller.dtos.TransacaoRequestDTO;
+import com.devraimundos.FlowAPI.infrastructure.exceptions.UnprocessableEntity;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class TransacaoService {
     private final List<TransacaoRequestDTO> listaTransacoes = new ArrayList<>();
+
+    log.info("Gravando Transações");
+
+    public void adicionarTransacoes(TransacaoRequestDTO dto) {
+        if (dto.dataHora().isAfter(OffsetDateTime.now())) {
+            throw new UnprocessableEntity("Data de hora invalido");
+        }
+    }
 
 }
